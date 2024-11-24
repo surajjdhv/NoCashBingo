183032a5154a14d5c7e7ac366ec19290ce1bc9fb
Akash Mahadik <akashpm1996@gmail.com>
2024-11-24 10:21:09 +0530

    Add tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,9 @@
 </script>
 
 <template>
-  <main>
+  <main class="min-h-screen bg-slate-200 flex justify-center items-center">
+    <section class=" max-w-[1360px] mx-auto">
+      <h1 class="text-6xl font-bold">Welcome to NoCashBingo</h1>
+    </section>
   </main>
 </template>
